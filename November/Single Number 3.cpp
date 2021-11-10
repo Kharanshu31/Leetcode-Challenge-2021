dfef9f8f5b72cae4c8ab4ebda459a4557d36c82a
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2021-11-10 16:30:31 +0530

    Day 6

---
+++ November/Single Number 3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        
+        vector<int>ans;
+        for(auto &it:m)
+        {
+            if(it.second==1)
+                ans.push_back(it.first);
+        }
+        
+        return ans;
+    }
+};
